0bc8a4922708d8d62443c8c915990f977eec0ba0
chelproc <chelproc@outlook.com>
2023-04-02 12:35:29 +0000

    本番環境でソースマップを生成しない

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -22,6 +22,7 @@ const config: GatsbyConfig = {
       },
     },
     "gatsby-plugin-sharp",
+    "gatsby-plugin-no-sourcemaps",
     "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
