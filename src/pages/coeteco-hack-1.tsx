a17881ed09f95f2b0a986e57a73492b25b250bf4
naka-12 <104970808+naka-12@users.noreply.github.com>
2023-11-12 18:45:02 +0900

    文面を修正

--- src/pages/coeteco-hack-1.tsx
+++ src/pages/coeteco-hack-1.tsx
@@ -10,7 +10,7 @@ export default function CoetecoHackathonPage() {
     <>
       <GlobalHeader />
       <div className="container mx-auto px-4 pt-16 pb-12">
-        <h1 className="text-4xl">コエテコHack #1 特設ページ</h1>
+        <h1 className="text-4xl">コエテコ Hack#1</h1>
       </div>
       <StaticImage
         alt=""
@@ -19,19 +19,43 @@ export default function CoetecoHackathonPage() {
       />
       <main className="container mx-auto px-4 py-16">
         <div className="prose max-w-none">
+          <h2>コエテコ Hack#1 とは</h2>
           <p>
-            本イベントは、高校生・大学生・大学院生を対象に参加者を募集します。「AI×教育」をテーマに、教育
-            現場における課題を最新の AI
-            技術を使って解決するプロダクトの制作を、4 人 1
-            組のチームになって挑戦
-            していただきます。最終日にはコンペティションを行い、上位 3
-            チームには賞金が授与されます。 『コエテコ Hack#1』は、「コエテコ
-            byGMO」と「ut.code();」の継続的な協力の第一歩として開催
-            し、今後も定期的に実施していく予定です。
+            {/* eslint-disable prettier/prettier */}
+            本イベントは、「コエテコ byGMO」と「ut.code();」が共催する、高校生・大学生・大学院生を対象としたハッカソンイベントです。
           </p>
           <p>
+            「AI×教育」をテーマとしたプロダクトの制作を、4人1組のチームになって挑戦していただきます。
+          </p>
+          <p>
+            最終日にはコンペティションを行い、上位3チームには賞金が授与されます。
+          </p>
+          <h2>『コエテコ Hack#1』の概要</h2>
+          <h3>開催日時</h3>
+          <p>2023年12月9日 (土)・10日 (日) 15:00 ~ 22:00</p>
+          <h3>開催場所</h3>
+          <p>GMOインターネットグループ本社 渋谷セルリアンタワー 11階コミュニケーションスペース「GMO Yours」</p>
+          <p>(東京都渋谷区桜丘町 26-1 セルリアンタワー11F)</p>
+          <h3>賞金</h3>
+          <p>総額 28 万円</p>
+          <ul>
+            <li>1位: 16万円</li>
+            <li>2位: 8万円</li>
+            <li>3位: 4万円</li>
+          </ul>
+          <h3>参加対象</h3>
+          <p>高校生、大学生、大学院生</p>
+          <h3>参加費</h3>
+          <p>無料</p>
+          <h3>定員</h3>
+          <p>50名 (抽選)</p>
+          <h3>申し込み方法</h3>
+          <p>下記より必要事項を入力してお申し込みください。</p>
+          <h3>締切</h3>
+          <p className="font-bold">12月1日 (金) 17:00</p>
+          <p>
             <div className="not-prose">
-              <ActionButton to="">
+              <ActionButton to="https://forms.gle/w6KVjyqAtrPKhW5z6">
                 {/* TODO: リンク先を追加 */}
                 参加申し込みフォーム
               </ActionButton>
@@ -45,5 +69,5 @@ export default function CoetecoHackathonPage() {
 }
 
 export function Head() {
-  return <CommonHead title="コエテコHack #1 特設ページ" />;
+  return <CommonHead title="コエテコ Hack#1 特設ページ" />;
 }
