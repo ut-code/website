3515d877058b22a320e340410e58d8e9727cabd2
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-11-19 22:49:08 +0900

    Change colors

--- src/pages/events/coeteco-hack-1.tsx
+++ src/pages/events/coeteco-hack-1.tsx
@@ -18,14 +18,14 @@ function AIAndEducationLogo(props: { className?: string }) {
     >
       <path
         d="M40 0C17.9086 0 0 17.9086 0 40V210C0 232.091 17.9086 250 40 250H210C232.091 250 250 232.091 250 210V204.381C210.537 199.46 180 165.796 180 125C180 84.2038 210.537 50.5399 250 45.619V40C250 17.9086 232.091 0 210 0H40Z"
-        fill="#7CCE5F"
+        className="fill-lime-400"
       />
       <text x="95" y="150" fontSize="70px" fontStyle="italic" fill="white">
         AI
       </text>
       <path
         d="M310 0C287.909 0 270 17.9086 270 40V65.8296C266.748 65.284 263.407 65 260 65C226.863 65 200 91.8629 200 125C200 158.137 226.863 185 260 185C263.407 185 266.748 184.716 270 184.17V210C270 232.091 287.909 250 310 250H480C502.091 250 520 232.091 520 210V40C520 17.9086 502.091 0 480 0H310Z"
-        fill="#6ECDE2"
+        className="fill-sky-400"
       />
       <text x="325" y="150" fontSize="70px" fontStyle="italic" fill="white">
         教育
@@ -42,10 +42,10 @@ function EventBannerSection() {
       <div className="w-full max-w-xs text-2xl md:text-xl">
         <div className="text-left">参加者でチームを組み</div>
         <div className="text-center py-3">
-          <div className="underline decoration-[#B1EFB0] decoration-8 underline-offset-1">
+          <div className="underline decoration-lime-200 decoration-8 underline-offset-1">
             最新の AI 技術を用いて
           </div>
-          <div className="underline decoration-[#B2ECF3] decoration-8 underline-offset-1">
+          <div className="underline decoration-sky-200 decoration-8 underline-offset-1">
             教育現場の課題に
           </div>
         </div>
@@ -60,7 +60,7 @@ function EventInfoItem(props: { title: string; children: ReactNode }) {
 
   return (
     <section className="flex flex-col md:flex-row text-left items-left space-y-2 md:space-y-0 md:space-x-10">
-      <h3 className="self-start bg-[#549C48] font-semibold text-white md:text-center w-full md:w-24 rounded-full px-6 md:px-4 py-2">
+      <h3 className="self-start bg-lime-700 font-semibold text-white md:text-center w-full md:w-24 rounded-full px-6 md:px-4 py-2">
         {title}
       </h3>
       <div className="flex w-full px-6 md:px-0 items-center">{children}</div>
@@ -70,8 +70,8 @@ function EventInfoItem(props: { title: string; children: ReactNode }) {
 
 function EventSummarySection() {
   return (
-    <section className="bg-[#F7FFE5] p-5 space-y-8 rounded-3xl">
-      <h2 className="text-[#008D17] font-bold text-3xl text-center">
+    <section className="bg-lime-100 p-5 space-y-8 rounded-3xl">
+      <h2 className="text-lime-700 font-bold text-3xl text-center">
         イベント概要
       </h2>
 
@@ -102,7 +102,7 @@ function EventSummarySection() {
       </div>
 
       <div
-        className="bg-[#C2ED8B] text-[#055A12] text-center items-center px-4 py-2 space-y-2"
+        className="bg-lime-300 text-lime-800 text-center items-center px-4 py-2 space-y-2"
         style={{
           clipPath:
             "polygon(0 0, 100% 0, 95% 50%, 100% 100%, 0 100%, 5% 50%, 0 0)",
@@ -125,7 +125,7 @@ function ParticipationCTASection() {
       <div className="text-xl w-full max-w-sm">
         <div className="text-left">
           アイデアを
-          <span className="text-2xl underline decoration-[#CBE495] decoration-8 underline-offset-1 inline-block px-1 py-2 -rotate-12">
+          <span className="text-2xl underline decoration-lime-200 decoration-8 underline-offset-1 inline-block px-1 py-2 -rotate-12">
             カタチ
           </span>
           にする喜びを
@@ -137,12 +137,12 @@ function ParticipationCTASection() {
           href="https://forms.gle/w6KVjyqAtrPKhW5z6"
           target="_blank"
           rel="noreferrer"
-          className="bg-[#ED9E28] font-semibold text-white text-xl w-full md:w-64 rounded-full inline-block px-8 py-4 hover:bg-amber-600 hover:shadow-lg hover:scale-105"
+          className="bg-amber-500 font-semibold text-white text-xl w-full md:w-64 rounded-full inline-block px-8 py-4 hover:bg-amber-600 hover:shadow-lg hover:scale-105"
           aria-label="イベントへの申し込みフォームへ"
         >
           お申し込みはこちら
         </a>
-        <div className="text-[#FF0000]">
+        <div className="text-red-500">
           <time dateTime="2023-12-01T17:00+09:00">
             12 月 1 日（金）17:00 〆
           </time>
