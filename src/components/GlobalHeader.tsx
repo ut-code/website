487eb12d1caaef98fd0e5fef8e5e308bead64f6b
aster <137767097+aster-void@users.noreply.github.com>
2024-05-15 13:34:28 +0900

    changed banner to may festival

--- src/components/GlobalHeader.tsx
+++ src/components/GlobalHeader.tsx
@@ -17,12 +17,12 @@ export default function GlobalHeader({ className }: { className?: string }) {
   return (
     <header className={clsx("relative", className)}>
       <div className="flex gap-4 justify-center items-center px-1 py-3 md:py-1 bg-green-400">
-        <span>新歓を開催します</span>
+        <span>五月祭に参加します</span>
         <Link
-          to="/articles/2024-welcome-events/"
+          to="https://mf97.utcode.net/"
           className="inline-flex items-center flex-shrink-0 gap-0.5 hover:underline"
         >
-          詳細はこちら
+          特設ウェブサイトへ
           <FiArrowRight />
         </Link>
       </div>
