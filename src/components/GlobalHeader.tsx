ca3cbab16ae4e30ba1f85c3cad76319e2fea8180
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-06-28 13:14:06 +0900

    前回のを参考に大枠を作成

--- src/components/GlobalHeader.tsx
+++ src/components/GlobalHeader.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Popover, Transition } from "@headlessui/react";
-import { /* FiArrowRight, */ FiMenu } from "react-icons/fi";
+import {  FiArrowRight,  FiMenu } from "react-icons/fi";
 import { Link } from "gatsby";
 import clsx from "clsx";
 import Logo from "./Logo";
@@ -16,18 +16,21 @@ const links = [
 export default function GlobalHeader({ className }: { className?: string }) {
   return (
     <header className={clsx("relative", className)}>
-      {/*
       <div className="flex gap-4 justify-center items-center px-1 py-3 md:py-1 bg-green-400">
-        <span>ハッカソンを開催します</span>
+        <span>
+          「AI×日常」
+          <span className="hidden sm:inline">をテーマとした</span>
+          ハッカソンを開催
+          <span className="hidden sm:inline">します</span>
+        </span>
         <Link
-          to="/articles/2024-june-hackathon/"
+          to="/events/coeteco-hack-2/"
           className="inline-flex items-center flex-shrink-0 gap-0.5 hover:underline"
         >
           詳細はこちら
           <FiArrowRight />
         </Link>
       </div>
-      */}
       <div className="container mx-auto h-16 flex">
         <Link to="/" className="flex items-center px-4">
           <Logo className="w-36" variant="default" />
