e22bf11330f6eec34269aeb0f83403d2b0094ec5
aster <137767097+aster-void@users.noreply.github.com>
2025-03-09 00:10:28 +0900

    fix tw config

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import("tailwindcss/defaultConfig")} */
-module.exports = {
-  content: ["./src/**/*.tsx"],
-  theme: { extend: { colors: { primary: { main: "#00d372" } } } },
-  variants: {},
-  // eslint-disable-next-line global-require
-  plugins: [require("@tailwindcss/typography")],
-};
