5af4d0c6f963aba7872a5bef867f7ab00406c42d
aster <137767097+aster-void@users.noreply.github.com>
2025-03-26 14:02:09 +0900

    perf: separate svg logos with img tag

--- src/islands/svelte/hamburger-button.svelte
+++ src/islands/svelte/hamburger-button.svelte
@@ -8,24 +8,7 @@
 <Popover.Root>
   <Popover.Trigger class="h-16 w-16 hover:bg-gray-100 lg:hidden">
     <div class="mx-auto my-auto h-6 w-6 text-gray-600">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="feather feather-menu"
-        ><line x1="3" y1="12" x2="21" y2="12"></line><line
-          x1="3"
-          y1="6"
-          x2="21"
-          y2="6"
-        ></line><line x1="3" y1="18" x2="21" y2="18"></line></svg
-      >
+      <img src="/icons/menu.svg" alt="open menu" />
     </div>
   </Popover.Trigger>
 
