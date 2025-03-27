56eff3e3171dc062c355cf95dbfa410fdc12115a
aster <137767097+aster-void@users.noreply.github.com>
2025-03-27 17:14:09 +0900

    change hamburger button style

--- src/islands/svelte/hamburger-button.svelte
+++ src/islands/svelte/hamburger-button.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <Popover.Root>
-  <Popover.Trigger class="h-16 w-16 hover:bg-gray-200/90 lg:hidden">
+  <Popover.Trigger class="h-16 w-16 hover:bg-gray-100 lg:hidden">
     <div class="mx-auto my-auto h-6 w-6 text-gray-600">
       <img src="/icons/menu.svg" alt="open menu" />
     </div>
@@ -15,11 +15,11 @@
   <Popover.Content align="end" side="bottom" strategy="fixed">
     <div
       transition:fade|global={{ duration: 70 }}
-      class="w-[100vw] bg-gray-50/70 transition-opacity duration-300 lg:hidden"
+      class="w-[100vw] bg-gray-50/70 backdrop-blur-lg transition-opacity duration-300 lg:hidden"
     >
       <ul>
         {#each links as link}
-          <li class="hover:bg-gray-200/90">
+          <li class="hover:bg-gray-100">
             <a
               href={link.href}
               class="block px-4 py-2"
