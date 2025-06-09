49c8af7cdd9d914d9f6418d4a02797c30e782e6f
aster <137767097+aster-void@users.noreply.github.com>
2025-06-09 17:59:31 +0900

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -4,16 +4,23 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    internal-helpers = {
+      url = "github:ut-code/internal-helpers";
+      inputs.nixpkgs.url = "nixpkgs";
+    };
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
+    internal-helpers,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      devShells.default = pkgs.callPackage ./shell.nix {};
+      devShells.default = pkgs.callPackage ./shell.nix {
+        inherit internal-helpers;
+      };
     });
 }
--- shell.nix
+++ shell.nix
@@ -1,9 +1,13 @@
-{pkgs ? import <nixpkgs> {}}:
+{
+  pkgs ? import <nixpkgs> {},
+  internal-helpers ? builtins.getFlake "github:ut-code/internal-helpers"
+}:
 pkgs.mkShell {
   packages = [
     pkgs.bun
     pkgs.nodejs-slim # required by astro to find sharp
     pkgs.ffmpeg
     pkgs.imagemagick
+    internal-helpers.packages.${pkgs.system}.disallow-large-dir
   ];
 }
